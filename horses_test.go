3dbef9e49b0d951e8e67b12d0f58f24a22cd3404
Pete Richardson <peterichardson@apple.com>
2020-02-23 16:36:46 -0800

    Fix horse and snake tests

--- horses_test.go
+++ horses_test.go
@@ -5,19 +5,19 @@ import (
 	"testing"
 )
 
-func TestNewDog(t *testing.T) {
-	p := NewDog("Hannibal", "Saint Bernard", 9)
-	assert.Equal(t, "Dog", p.Species)
+func TestNewHorse(t *testing.T) {
+	p := NewHorse("Hannibal", "Saint Bernard", 9)
+	assert.Equal(t, "Horse", p.Species)
 	assert.Equal(t, "Hannibal", p.Name)
 	assert.Equal(t, "Saint Bernard", p.Breed)
 	assert.Equal(t, 9, p.Age)
 }
 
-func TestRandomDog(t *testing.T) {
+func TestRandomHorse(t *testing.T) {
 	d1 := RandomCat()
 	d2 := RandomCat()
 	//log.Println(d1)
 	//log.Println(d2)
 	// Assert that the pets are not identical
-	assert.NotEqual(t, d1, d2, "Two random dogs seem to be identical.")
+	assert.NotEqual(t, d1, d2, "Two random horses seem to be identical.")
 }
